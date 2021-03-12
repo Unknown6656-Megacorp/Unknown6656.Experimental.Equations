c4d90a8465441e21c370e80f7f034f92db2ee8a3
unknown6656 <unknown84@gmx.de>
2021-03-12 20:24:18 +0100

    initial code commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Unknown6656.Experimental.Equations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
