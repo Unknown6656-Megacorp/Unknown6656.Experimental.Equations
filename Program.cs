7baca8ce7b2ab4ea1bb70e4c527a7b42215db799
unknown6656 <unknown84@gmx.de>
2021-03-12 21:03:24 +0100

    uncommented code

--- Program.cs
+++ Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
 
-
 Console.WriteLine("Hello World!");
 
 
 
 
-
-
-
-public abstract class Equation
-{
-
-}
+// 
+// 
+// 
+// 
+// public static class Equation
+// {
+// }
+// 
+// public abstract record Expression
+// {
+//     public sealed record Variable(string Name) : Expression;
+//     public sealed record Constant(double Value) : Expression;
+//     public sealed record Sum(Expression Operand1, Expression Operand2) : Expression;
+// 
+// 
+//     public abstract double EvaluateAt(double value);
+// }
+// 
+// 
+// 
+// 
+// // 3x - 4 = 2
+// // 3x = 2 + 4
+// // 3x = 6
+// // x = 6 / 3
+// // x = 2
+// 
