3216f3b56ba8a573101cef31d0031472201dbed1
unknown6656 <unknown84@gmx.de>
2021-03-12 21:03:52 +0100

    began adding expression tree

---
+++ Unknown6656.Experimental.Equations.FSharp/Equation.fs
@@ -0,0 +1,31 @@
+﻿module Equation
+
+
+type Solution =
+    | None
+    | Some of float list
+    | All
+
+type Expression =
+    | Variable
+    | Const of float
+    | Sum of  Expression * Expression
+    with
+        member x.IsConstant =
+            match x with
+            | Variable -> false
+            | Const _ -> true
+            | Sum (a, b) -> a.IsConstant && b.IsConstant
+
+        member this.EvaluateAt x =
+            match this with
+            | Variable -> x
+            | Const c -> c
+            | Sum (a, b) -> (a.EvaluateAt x) + (b.EvaluateAt x)
+
+        member x.SolveFor result =
+            match x with
+            | Variable -> Some [result]
+            | Const c when c = result -> All
+            | Const _ -> None
+            | Sum (a, b) -> failwith "<todo>"
