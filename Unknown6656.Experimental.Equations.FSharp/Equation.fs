c8ab801407c33d84792853be1bba63626af0dd9f
unknown6656 <unknown84@gmx.de>
2021-03-13 02:44:15 +0100

    continued expanding the expression system

--- Unknown6656.Experimental.Equations.FSharp/Equation.fs
+++ Unknown6656.Experimental.Equations.FSharp/Equation.fs
@@ -5,6 +5,8 @@ let inline (||>>) (f, g) (x, y) = (f x, g y)
 
 let inline (?) c a b = if c then a else b
 
+let inline (|Contains|_|) list item = if List.contains item list then Some item else None
+
 let separate (selector : 'a -> Choice<'b, 'c>) =
     let rec intern acc = function
                          | [] -> acc
@@ -24,91 +26,146 @@ type Solution =
 type Expression =
     | Variable
     | Const of float
-    | Sum of Expression list
-    | Product of Expression list
-    // | Difference of Expression * Expression
-    // | Quotient of Expression * Expression
-    // | Power of Expression * Expression
+    | Negation of Expression
+    | Reciprocal of Expression
+    | Sum of Set<Expression>
+    | Product of Set<Expression>
+    | Exponentiation of Expression * Expression
     with
+        static member Difference a b = seq { a; Negation b }
+                                       |> set
+                                       |> Sum
+
+        static member Division a b = seq { a; Reciprocal b }
+                                     |> set
+                                     |> Product
+
         override this.ToString() =
             match this with
             | Variable -> "x"
             | Const x -> x.ToString()
+            | Negation x -> sprintf "-(%O)" x
+            | Reciprocal x -> sprintf "1/(%O)" x
             | Sum xs -> xs
-                        |> List.map (fun x -> x.ToString())
+                        |> Seq.map (fun x -> x.ToString())
                         |> String.concat " + "
                         |> sprintf "(%s)"
             | Product xs -> xs
-                            |> List.map (fun x -> x.ToString())
+                            |> Seq.map (fun x -> x.ToString())
                             |> String.concat " * "
                             |> sprintf "(%s)"
+            | Exponentiation (a, b) -> sprintf "(%O)^(%O)" a b
 
         member x.IsConstant =
             match x with
             | Variable -> false
-            | Const _
-            | Sum [] | Product [] -> true
-            | Sum xs | Product xs -> List.forall (fun (x : Expression) -> x.IsConstant) xs
-
-        member x.Fold =
-            let try_fold op expr initial xs =
-                let c, v = xs
-                            |> separate (fun (x : Expression) ->
-                                match x.Fold with
-                                | Const c -> Choice1Of2 c
-                                | _ -> Choice2Of2 x
-                            )
-                            ||> (List.fold op initial >> Const, fun vars -> vars)
-
-
-                // match a.Fold, b.Fold with
-                // | Const a', Const b' -> Const (op a' b')
-                // | t -> expr t
-                
-            let original = x
-            match x with
-            | Variable
-            | Const _ -> x
-            | Sum xs -> try_fold (+) Sum xs
-            | Product xs -> try_fold (*) Product xs
-            |> function
-
-
-            | Sum (a, b) when a = b -> Product (Const 2.0, a)
-
-            | Sum (Const a, Sum (Const b, c))
-            | Sum (Const a, Sum (c, Const b))
-            | Sum (Sum (Const b, c), Const a)
-            | Sum (Sum (c, Const b), Const a) -> Sum(Const(a + b), c)
+            | Const _ -> true
+            | Sum xs when xs.Count = 0 -> true
+            | Product xs when xs.Count = 0 -> true
+            | Sum xs | Product xs -> Seq.forall (fun (x : Expression) -> x.IsConstant) xs
+            | Reciprocal x
+            | Negation x -> x.IsConstant
+            | Exponentiation(a, b) -> a.IsConstant && b.IsConstant
             
-
-            | Sum (a, Sum (b, c)) when a = b -> Product(Const 2.0 )
-            | Sum (a, Sum (c, b)) when a = b -> 
-
-
-
-            | Product (Const a, Product (Const b, c))
-            | Product (Const a, Product (c, Const b))
-            | Product (Product (Const b, c), Const a)
-            | Product (Product (c, Const b), Const a) -> Product(Const(a * b), c)
-
-            | a -> a
-            |> fun a -> if a = original then a else a.Fold
-
         member this.EvaluateAt x =
             match this with
             | Variable -> x
             | Const c -> c
-            | Sum (a, b) -> (a.EvaluateAt x) + (b.EvaluateAt x)
-            | Product (a, b) -> (a.EvaluateAt x) * (b.EvaluateAt x)
+            | Negation n -> -(n.EvaluateAt x)
+            | Reciprocal r -> 1.0 / (r.EvaluateAt x)
+            | Exponentiation (a, b) -> (a.EvaluateAt x) ** (b.EvaluateAt x)
+            | Sum xs -> xs |> Seq.fold (fun sum elem -> sum + elem.EvaluateAt x) 0.0
+            | Product xs -> xs |> Seq.fold (fun product elem -> product * elem.EvaluateAt x) 1.0
+
+        member x.Fold =
+            let separate = 
+                Seq.toList >> separate (fun (x : Expression) ->
+                                            match x.Fold with
+                                            | Const c -> Choice1Of2 c
+                                            | _ -> Choice2Of2 x
+                                        )
+
+            let rec fold_sum items =
+                let consts, vars = separate items
+                let vars = vars
+                           |> List.collect (function Sum xs -> fold_sum xs | x -> [x])
+                           |> List.groupBy id
+                           |> List.map (fun (e, l) ->
+                                            if l.Length > 1 then
+                                                [Const (float l.Length); e]
+                                                |> set
+                                                |> Product
+                                            else
+                                                e)
+                (consts
+                |> List.fold (+) 0.0
+                |> Const)
+                :: vars
+                
+            let rec fold_product items =
+                let consts, vars = separate items
+                let vars = vars
+                            |> List.collect (function Product xs -> fold_product xs | x -> [x])
+                            |> List.groupBy id
+                            |> List.map (fun (e, l) -> if l.Length > 1 then Exponentiation (e, Const (float l.Length)) else e)
+                (consts
+                |> List.fold (*) 1.0
+                |> Const)
+                :: vars
+
+            let original = x
+            let folded = match x with
+                         | Variable
+                         | Const _ -> x
+                         | Reciprocal x -> Reciprocal x.Fold
+                         | Negation x -> Negation x.Fold
+                         | Sum xs -> (fold_sum >> set >> Sum) xs
+                         | Product xs -> (fold_product >> set >> Product) xs
+                         | Exponentiation (a, b) -> Exponentiation (a.Fold, b.Fold)
+                         |> function
+                         | Negation (Const x) -> Const -x
+                         | Reciprocal (Const x) -> Const (1.0 / x)
+                         | Negation (Negation x)
+                         | Reciprocal (Reciprocal x)
+                         | Exponentiation (x, Const 1.0) -> x
+                         | Exponentiation (_, Const 0.0)
+                         | Exponentiation (Const 1.0, _) -> Const 1.0
+                         | Exponentiation (Const 0.0, _) -> Const 0.0
+                         | Exponentiation (x, Negation y) -> Reciprocal (Exponentiation (x, y))
+                         | Exponentiation (x, Const y) when y < 0.0 -> Reciprocal (Exponentiation (x, Const -y))
+
+                         | Sum xs ->
+                            if xs.Count = 0 then Const 0.0
+                            elif xs.Count = 1 then Seq.head xs
+                            else
+                                ()
+                        
+                         | Product xs ->
+                            if xs.Count = 0 then Const 1.0
+                            elif xs.Count = 1 then Seq.head xs
+                            else
+                                ()
+
+                         
+                         //| Sum xs ->
+                         //   match xs with
+                         //   | Contains (Sum s2) -> Const 0.0
+
+
+                         | a -> a
+
+            if folded = original then
+                folded
+            else
+                folded.Fold
 
         member x.SolveFor result =
             match x.Fold with
             | Variable -> Some [result]
             | Const c when c = result -> All
             | Const _ -> None
-            | Sum (Const a, b) -> b.SolveFor (result - a)
-            | Sum (a, Const b) -> a.SolveFor (result - b)
-            | Product (Const a, b) -> b.SolveFor (result / a)
-            | Product (a, Const b) -> a.SolveFor (result / b)
+            //| Sum (Const a, b) -> b.SolveFor (result - a)
+            //| Sum (a, Const b) -> a.SolveFor (result - b)
+            //| Product (Const a, b) -> b.SolveFor (result / a)
+            //| Product (a, Const b) -> a.SolveFor (result / b)
             | a -> failwithf "TODO: %O" a
--- Unknown6656.Experimental.Equations.FSharp/Program.fs
+++ Unknown6656.Experimental.Equations.FSharp/Program.fs
@@ -5,7 +5,7 @@ open Equation
 
 [<EntryPoint>]
 let main argv =
-    let e1 = Sum(Sum(Const 3.0, Variable), Variable)
+    let e1 = Sum[Const 3.0; Variable; Sum[Const 5.0; Negation Variable; Reciprocal Variable]]
     let e2 = e1.Fold
     let res = e2.EvaluateAt 5.0
     let x = e2.SolveFor res
